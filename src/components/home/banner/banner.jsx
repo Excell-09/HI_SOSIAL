d667d55206776ee98ea95a58fbe808d45f8c2b6d
excell-09 <excell.agcb12@gmail.com>
2023-09-19 19:09:44 +0700

    update:banner home

--- src/components/home/banner/banner.jsx
+++ src/components/home/banner/banner.jsx
@@ -3,15 +3,10 @@ import { IoIosArrowForward } from "../../../../node_modules/react-icons/io";
 import HighligtBanner from "./highligt-banner/highligtBanner";
 
 const Banner = ({ t }) => {
-  const a =
-    "px-5 grid gap-5 grid-cols-1 md:items-center md:grid-cols-2 lg:justify-items-end lg:items-start lg:grid-cols-7 md:pe-0 lg:gap-16";
   return (
     <section className="lg:mt-[57px]">
       <div className="flex px-5 flex-col lg:flex-row gap-5 lg:-16">
-        {/* font-plus-jakarta-sans flex justify-center lg:justify-end lg:col-span-3  */}
-
         <div className="flex-1 font-plus-jakarta-sans">
-          {/* md:w-full lg:w-4/5 lg:space-y-14  */}
           <div className="md:w-full lg:w-4/5 lg:mt-10">
             <h1 className="font-clash-display text-4xl font-medium mb-7 lg:text-5xl">
               {t("Home.banner.title")}
@@ -29,19 +24,14 @@ const Banner = ({ t }) => {
           </div>
         </div>
 
-        {/* lg:col-span-4 */}
         <div className="flex-1 flex flex-col overflow-hidden">
           <img
             src={bannerImage}
             className="md:h-[450px] md:w-full lg:h-[424px] lg:w-[570px] object-cover rounded-lg mb-[10px]"
           />
-          <HighligtBanner t={t} className={"md:w-full"}/>
+          <HighligtBanner t={t} className={"md:w-full"} />
         </div>
       </div>
-
-      {/* <div className="mt-2 md:mt-8 lg:-mt-28 flex flex-col items-center">
-        <HighligtBanner t={t} />
-      </div> */}
     </section>
   );
 };
--- src/components/home/banner/highligt-banner/highligtBanner.jsx
+++ src/components/home/banner/highligt-banner/highligtBanner.jsx
@@ -1,13 +1,12 @@
 const HighligtBanner = ({ t, className }) => {
   return (
-    // ps-5  bg-blue-500 text-white rounded-3xl w-11/12 h-44 grid grid-cols-1 justify-items-center items-center md:grid-cols-5 md:h-28 md:gap-2 lg:gap-8  lg:w-[1180px] lg:h-[204.742px]
     <section
-      className={` px-5 py-7 bg-blue-500 text-white rounded-lg ${className}`}>
-      <p className="font-clash-display text-xl font-medium md:text-2xl lg:text-5xl md:col-span-2  lg:hidden">
+      className={`px-5 py-7 bg-blue-500 text-white rounded-lg ${className}`}>
+      <p className="font-clash-display text-xl font-medium md:text-2xl lg:text-5xl md:col-span-2 lg:hidden text-center">
         {t("Home.banner.highlight-banner")}
       </p>
 
-      <div className="text-center flex flex-col md:flex-row md:space-x-4 md:col-span-3 lg:space-x-7 justify-between">
+      <div className="flex flex-col md:flex-row md:space-x-4 md:col-span-3 lg:space-x-7 justify-between items-center mt-10 lg:mt-0">
         <div className="flex gap-5 items-center md:flex-col">
           <p className="text-2xl font-semibold md:text-4xl lg:text-[50px]">
             05+
