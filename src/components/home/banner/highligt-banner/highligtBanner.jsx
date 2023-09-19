3c6a9ca10438a87f72f3b16edf62cfebebe4a4ca
excell-09 <excell.agcb12@gmail.com>
2023-09-19 19:03:06 +0700

    update:banner highlght

--- src/components/home/banner/highligt-banner/highligtBanner.jsx
+++ src/components/home/banner/highligt-banner/highligtBanner.jsx
@@ -3,13 +3,12 @@ const HighligtBanner = ({ t, className }) => {
     // ps-5  bg-blue-500 text-white rounded-3xl w-11/12 h-44 grid grid-cols-1 justify-items-center items-center md:grid-cols-5 md:h-28 md:gap-2 lg:gap-8  lg:w-[1180px] lg:h-[204.742px]
     <section
       className={` px-5 py-7 bg-blue-500 text-white rounded-lg ${className}`}>
-      <p className="font-clash-display text-xl font-medium md:text-2xl lg:text-5xl md:col-span-2  ">
+      <p className="font-clash-display text-xl font-medium md:text-2xl lg:text-5xl md:col-span-2  lg:hidden">
         {t("Home.banner.highlight-banner")}
       </p>
 
-      <div className="text-center flex flex-col md:flex-row md:space-x-4 md:col-span-3 lg:space-x-7 lg:justify-between">
-        {/* flex gap-5 items-center md:flex-col */}
-        <div>
+      <div className="text-center flex flex-col md:flex-row md:space-x-4 md:col-span-3 lg:space-x-7 justify-between">
+        <div className="flex gap-5 items-center md:flex-col">
           <p className="text-2xl font-semibold md:text-4xl lg:text-[50px]">
             05+
           </p>
@@ -17,8 +16,7 @@ const HighligtBanner = ({ t, className }) => {
             {t("Home.banner.highlight-1")}
           </p>
         </div>
-        {/* flex gap-5 items-center md:flex-col md:gap-5 lg:gap-10 */}
-        <div>
+        <div className="flex gap-5 items-center md:flex-col">
           <p className="text-2xl font-semibold md:text-4xl lg:text-[50px]">
             05+
           </p>
@@ -26,8 +24,7 @@ const HighligtBanner = ({ t, className }) => {
             {t("Home.banner.highlight-2")}
           </p>
         </div>
-        {/* flex gap-5 items-center md:flex-col md:gap-5 lg:gap-10 */}
-        <div>
+        <div className="flex gap-5 items-center md:flex-col">
           <p className="text-2xl font-semibold md:text-4xl lg:text-[50px]">
             02+
           </p>
