88d9f463cca949971781f75bb8acd35bd3a13742
excell-09 <excell.agcb12@gmail.com>
2023-09-19 19:43:19 +0700

    update:ourwork

--- src/components/home/our-work/ourWork.jsx
+++ src/components/home/our-work/ourWork.jsx
@@ -7,15 +7,34 @@ const OurWork = ({ t }) => {
   return (
     <section className="md:grid md:grid-cols-2 md:gap-x-10 md:justify-items-end md:items-center lg:gap-x-14 ">
       <div>
-        <h1 className="mb-3 font-clash-display text-2xl font-medium md:text-3xl lg:mb-12 lg:text-5xl">{t("Home.our-work.text-one")}</h1>
+        <h1 className="font-clash-display text-2xl font-medium md:text-3xl lg:text-5xl mb-[56px]">
+          {t("Home.our-work.text-one")}
+        </h1>
 
-        <ListExperience title={t("Home.our-work.title-1")} text={t("Home.our-work.descript-1")} img={shape1} />
+        <div className="flex flex-col lg:gap-10 md:gap-8">
+          <ListExperience
+            title={t("Home.our-work.title-1")}
+            text={t("Home.our-work.descript-1")}
+            img={shape1}
+          />
 
-        <ListExperience title={t("Home.our-work.title-2")} text={t("Home.our-work.descript-2")} img={shape2} />
+          <ListExperience
+            title={t("Home.our-work.title-2")}
+            text={t("Home.our-work.descript-2")}
+            img={shape2}
+          />
 
-        <ListExperience title={t("Home.our-work.title-3")} text={t("Home.our-work.descript-3")} img={shape3} />
+          <ListExperience
+            title={t("Home.our-work.title-3")}
+            text={t("Home.our-work.descript-3")}
+            img={shape3}
+          />
+        </div>
       </div>
-      <img className="hidden md:block" src={experienceImg} />
+      <img
+        className="hidden md:block  h-full object-cover rounded-2xl"
+        src={experienceImg}
+      />
     </section>
   );
 };
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,20 @@
-import { Banner, OurWork, SeveralService, OurProject, Testi, Pricing, OtherService, OurPatner } from "../../components/home/index";
+import {
+  Banner,
+  OurWork,
+  SeveralService,
+  OurProject,
+  Testi,
+  Pricing,
+  OtherService,
+  OurPatner,
+} from "../../components/home/index";
 
 const Home = ({ t }) => {
   return (
     <section className="pt-36 md:pt-30 lg:pt-20 flex flex-col items-center max-w-[1110px] m-auto">
       <div className="max-w-[1920px] flex flex-col items-center justify-center space-y-12 md:space-y-24 lg:space-y-32">
         <Banner t={t} />
-        <div className="px-5 md:w-auto lg:w-10/12  md:flex md:flex-col md:items-center lg:block space-y-14 md:space-y-24 lg:space-y-32 ">
+        {/* <div className="px-5 md:w-auto lg:w-10/12  md:flex md:flex-col md:items-center lg:block space-y-14 md:space-y-24 lg:space-y-32 "> */}
           <OurWork t={t} />
           <div className="md:w-10/12 lg:w-auto space-y-14 md:space-y-24 lg:space-y-32 ">
             <SeveralService t={t} />
@@ -15,7 +24,7 @@ const Home = ({ t }) => {
             <Pricing t={t} />
             <OurPatner t={t} />
           </div>
-        </div>
+        {/* </div> */}
       </div>
     </section>
   );
