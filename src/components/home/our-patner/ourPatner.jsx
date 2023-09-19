7d62550cc8cef79eab7960de464e050317a97e9e
excell-09 <excell.agcb12@gmail.com>
2023-09-19 20:51:56 +0700

    update:ourPartner

--- src/components/home/our-patner/ourPatner.jsx
+++ src/components/home/our-patner/ourPatner.jsx
@@ -2,12 +2,16 @@ import { eduwork, elvron, sisko, upscale } from "../../../assets/patner/index";
 import OurPatnerCard from "./our-patner-card/ourPatnerCard";
 const OurPatner = ({ t }) => {
   return (
-    <section className="flex flex-col items-center justify-center md:flex-row md:justify-between space-y-5 md:space-x-2">
+    <section className="flex flex-col items-center justify-center md:flex-row md:justify-between space-y-5 md:space-x-2 container">
       <div className="space-y-3">
-        <div className="w-full">
-          <h1 className="font-clash-display font-medium text-2xl md:text-4xl lg:text-5xl">{t("Home.our-patner.title")}</h1>
+        <div className="w-full text-sky-500">
+          <h1 className="font-clash-display font-medium text-2xl md:text-4xl lg:text-5xl">
+            {t("Home.our-patner.title")}
+          </h1>
         </div>
-        <p className="md:text-xl lg:text-3xl md:w-80 lg:w-[500px] 2xl:w-[601px]">{t("Home.our-patner.text")}</p>
+        <p className="md:text-xl lg:text-2xl md:w-80 lg:w-[500px] 2xl:w-[601px] text-gray-500">
+          {t("Home.our-patner.text")}
+        </p>
       </div>
       <div className="grid grid-cols-2 gap-7 2xl:gap-x-14">
         <OurPatnerCard img={eduwork} />
