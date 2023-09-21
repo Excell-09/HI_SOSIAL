e3e8a0f1bd6562b46e6969aab9b36cb54dde9d9f
excell-09 <excell.agcb12@gmail.com>
2023-09-21 13:12:16 +0700

    update:other service card

--- src/components/home/other-service/other-service-card/otherServiceCard.jsx
+++ src/components/home/other-service/other-service-card/otherServiceCard.jsx
@@ -1,17 +1,17 @@
 const OtherServiceCard = ({ image, otherServices, t }) => {
   return (
-    <section className="relative flex-grow flex flex-col items-center ">
+    <section className="relative flex-grow flex flex-col items-center h-full w-full">
       <div className="gradient-img-otherService rounded-3xl absolute  w-full h-full" />
       <img
         src={image}
         alt="OtherService"
-        className="object-cover w-full h-[300px] rounded-3xl "
+        className="object-cover w-full h-full rounded-3xl "
       />
-      <div className="relative font-plus-jakarta-sans z-10 bg-white flex justify-around items-center shadow-md rounded-b-lg px-5 -mt-7 w-full h-14 md:h-16 md:-mt-10 lg:-mt-24 lg:rounded-full">
-        <p className="font-semibold text-sm lg:text-2xl flex-1 text-gray-500">
+      <div className="relative font-plus-jakarta-sans z-10 bg-white flex justify-around items-center shadow-md rounded-b-lg px-5 py-3 -mt-7 w-full lg:w-11/12 md:-mt-10 lg:-mt-24 lg:rounded-full">
+        <p className="font-semibold text-sm lg:text-xl flex-1 text-gray-600">
           {otherServices}
         </p>
-        <button className="btn-blue rounded-full text-xs lg:text-sm w-[120px] lg:w-[138px]">
+        <button className="btn-blue rounded-full text-xs lg:text-sm w-[120px] lg:w-[138px] shadow-lg">
           {t("Home.other-service.btn-text")}
         </button>
       </div>
