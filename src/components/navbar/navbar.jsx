8eeed09d4a3bea1963e70f250a5f2b96dcbe08ed
excell-09 <excell.agcb12@gmail.com>
2023-09-22 15:04:22 +0700

    update:animation navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = ({ id, en }) => {
 
   return (
     <div className="shadow-xl fixed w-full top-0 z-[999999] font-plus-jakarta-sans bg-white">
-      <div className="font-plus-jakarta-sans flex justify-between lg:justify-between items-center AppContainer bg-white py-3  ">
+      <div className="font-plus-jakarta-sans flex justify-between items-center AppContainer bg-white py-3">
         <img src={logo} className="cursor-pointer" />
         <div
           className="text-2xl border-2 border-black p-1 rounded hover:bg-gray-300 lg:opacity-0 cursor-pointer"
@@ -23,7 +23,7 @@ const Navbar = ({ id, en }) => {
           {open === false ? <AiOutlineMenu /> : <GrClose />}
         </div>
         <div
-          className={`absolute shadow-xl py-3 md:py-5 px-5 left-0 top-20 w-full transition duration-700 lg:static lg:translate-y-0 bg-white -z-10 ${
+          className={`absolute shadow-xl -z-10 py-3 md:py-5 px-5 left-0 top-20 w-full transition duration-700 lg:static lg:translate-y-0 bg-white lg:z-0 lg:py-0 lg:px-0 lg:shadow-none lg:ml-auto lg:max-w-max lg:flex lg:items-center ${
             open ? "translate-y-0" : "-translate-y-96"
           }`}>
           <ul className="flex flex-col cursor-pointer lg:flex-row gap-4 lg:items-center lg:me-10">
@@ -63,7 +63,7 @@ const Navbar = ({ id, en }) => {
           </ul>
           <button
             type="button"
-            className="w-32 btn-blue rounded-lg lg:rounded-full text-sm mt-3 lg:text-base lg:mt-auto lg:mx-10 lg:w-auto lg:hidden">
+            className="w-32 btn-blue lg:rounded-full text-sm mt-3 lg:text-base lg:mt-auto lg:ml-10 lg:w-auto">
             Contact Us
           </button>
         </div>
