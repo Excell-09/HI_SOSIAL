4f658072f8382b70703abc3cc07b4c7844d07092
excell-09 <excell.agcb12@gmail.com>
2023-09-22 14:51:35 +0700

    update:navbar animation

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -14,22 +14,20 @@ const Navbar = ({ id, en }) => {
   let selecLang = localStorage.getItem("lang") || "id";
 
   return (
-    <div className="py-3 shadow-xl fixed w-full top-0 bg-white z-[999999] font-plus-jakarta-sans">
-      <div className="flex justify-between lg:justify-between items-center font-plus-jakarta-sans max-w-[1110px] m-auto">
-        <img src={logo} className="cursor-pointer ms-4 " />
+    <div className="shadow-xl fixed w-full top-0 z-[999999] font-plus-jakarta-sans bg-white">
+      <div className="font-plus-jakarta-sans flex justify-between lg:justify-between items-center AppContainer bg-white py-3  ">
+        <img src={logo} className="cursor-pointer" />
         <div
-          className="me-10 text-2xl border-2 border-black p-1 rounded hover:bg-gray-300 lg:opacity-0 cursor-pointer"
+          className="text-2xl border-2 border-black p-1 rounded hover:bg-gray-300 lg:opacity-0 cursor-pointer"
           onClick={() => handleOpen()}>
           {open === false ? <AiOutlineMenu /> : <GrClose />}
         </div>
         <div
-          className={
-            !open
-              ? "flex flex-col absolute top-20  py-2 ps-4 w-full shadow-lg transition duration-700 opacity-0 lg:flex-row lg:opacity-100 lg:top-0 lg:static lg:w-auto lg:shadow-none"
-              : "flex flex-col absolute top-20  py-2 ps-4 w-full shadow-lg transition duration-700 bg-white"
-          }>
-          <ul className="flex flex-col cursor-pointer lg:flex-row lg:space-x-8 lg:w-auto lg:items-center lg:me-10">
-            <li className=" hover:text-gray-400">
+          className={`absolute shadow-xl py-3 md:py-5 px-5 left-0 top-20 w-full transition duration-700 lg:static lg:translate-y-0 bg-white -z-10 ${
+            open ? "translate-y-0" : "-translate-y-96"
+          }`}>
+          <ul className="flex flex-col cursor-pointer lg:flex-row gap-4 lg:items-center lg:me-10">
+            <li className="hover:text-gray-400">
               <Link to="/" onClick={() => (window.location.href = "#top")}>
                 Home
               </Link>
@@ -65,7 +63,7 @@ const Navbar = ({ id, en }) => {
           </ul>
           <button
             type="button"
-            className="w-32 btn-blue rounded-full text-sm mt-1 lg:text-base lg:mt-auto lg:mx-10 lg:w-auto ">
+            className="w-32 btn-blue rounded-lg lg:rounded-full text-sm mt-3 lg:text-base lg:mt-auto lg:mx-10 lg:w-auto lg:hidden">
             Contact Us
           </button>
         </div>
