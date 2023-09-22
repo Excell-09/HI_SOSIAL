66a8925db38f26aaa59a9584531b7194ad8fac0d
excell-09 <excell.agcb12@gmail.com>
2023-09-22 13:58:54 +0700

    update:highlight benner

--- src/components/home/banner/highligt-banner/highligtBanner.jsx
+++ src/components/home/banner/highligt-banner/highligtBanner.jsx
@@ -5,30 +5,32 @@ const HighligtBanner = ({ t, className }) => {
         {t("Home.banner.highlight-banner")}
       </p>
 
-      <div className="flex flex-col md:flex-row md:space-x-4 md:col-span-3 lg:space-x-7 justify-between items-center mt-10 lg:mt-0">
-        <div className="flex gap-5 items-center md:flex-col">
-          <p className="text-2xl font-semibold md:text-4xl lg:text-[50px]">
-            05+
-          </p>
-          <p className="text-base lg:text-sm lg:text-center whitespace-nowrap">
-            {t("Home.banner.highlight-1")}
-          </p>
-        </div>
-        <div className="flex gap-5 items-center md:flex-col">
-          <p className="text-2xl font-semibold md:text-4xl lg:text-[50px]">
-            05+
-          </p>
-          <p className="text-base lg:text-sm whitespace-nowrap">
-            {t("Home.banner.highlight-2")}
-          </p>
-        </div>
-        <div className="flex gap-5 items-center md:flex-col">
-          <p className="text-2xl font-semibold md:text-4xl lg:text-[50px]">
-            02+
-          </p>
-          <p className="text-base lg:text-sm whitespace-nowrap">
-            {t("Home.banner.highlight-3")}
-          </p>
+      <div className="mt-5 lg:mt-0 grid">
+        <div className="mx-auto lg:mx-0 flex flex-col md:flex-row md:space-x-4 md:col-span-3 lg:space-x-7 justify-between items-start">
+          <div className="flex gap-5 items-center md:flex-col">
+            <p className="text-2xl font-semibold md:text-4xl lg:text-[50px]">
+              05+
+            </p>
+            <p className="text-base lg:text-sm lg:text-center whitespace-nowrap">
+              {t("Home.banner.highlight-1")}
+            </p>
+          </div>
+          <div className="flex gap-5 items-center md:flex-col">
+            <p className="text-2xl font-semibold md:text-4xl lg:text-[50px]">
+              05+
+            </p>
+            <p className="text-base lg:text-sm whitespace-nowrap">
+              {t("Home.banner.highlight-2")}
+            </p>
+          </div>
+          <div className="flex gap-5 items-center md:flex-col">
+            <p className="text-2xl font-semibold md:text-4xl lg:text-[50px]">
+              02+
+            </p>
+            <p className="text-base lg:text-sm whitespace-nowrap">
+              {t("Home.banner.highlight-3")}
+            </p>
+          </div>
         </div>
       </div>
     </section>
